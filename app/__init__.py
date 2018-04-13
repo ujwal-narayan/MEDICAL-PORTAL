1419ca017407127759f20f252032aab5f22e4729
Sravani Boinepelli <sravani.boinepelli@gmail.com>
2018-04-13 10:34:22 +0530

    some more files that got effected as a result of rename. Now everyting should work as before

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static" )
 app.config['SECRET_KEY'] = os.environ.get(
     'SECRET_KEY') or 'you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
