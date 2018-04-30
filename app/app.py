309f47446d10b9e6204e9adc6a9e4b4a07f51e03
ujwal-narayan <ujwalnarayann@gmail.com>
2018-05-01 02:51:22 +0530

    about done

--- app/app.py
+++ app/app.py
@@ -87,6 +87,10 @@ def home():
                 return render_template('index.html')
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
